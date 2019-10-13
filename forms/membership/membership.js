49a9113bf5104bd73eb7a915826bf84c846b51dc
eliasrios7 <55107200+eliasrios7@users.noreply.github.com>
2019-10-13 18:45:46 -0500

    Updates
    Answered questions 3-6.

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
 /*
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
+memberSubmit = ""
 
+
+bttnSubmit.onclick=function(){
+  memberSubmit = inptFirstName.value
+  if (memberSubmit === "Mary" || "Sam" || "Joey" || "Paul" || "Mike" || "Char" || "Jenny" || "Kennedy" || "Kaden" || "Kurt" || "Anne")
+    lblMemberMessage.value = "You are a member!"
+  else    
+    lblMemberMessage.value = "You were not previously a member. Your name has been added to the list."
+    members.pop(memberSubmit)
+}
+*/
